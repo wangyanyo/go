3033e676ff5a9824f7139ed1d2136cbf6c6f1849
2378130965 <2378130965@MacBook-Pro-101.local>
2024-05-11 03:53:41 -0700

    new version

--- go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/wangyanyo/go.git
-
-go 1.22.3
-
-require github.com/wangyanyo/test v0.0.0-20240511100105-e15cc0e4a049 // indirect
--- main.go
+++ main.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/wangyanyo/test"
 )
 
 func main() {
-	fmt.Println(test.Calc(1, 1))
+	fmt.Println("hahaha")
 }
